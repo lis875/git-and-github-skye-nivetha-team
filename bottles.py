424ff94d53f5994813759497d2034aa1e59e98f5
Sherlenbon <dbai22@wisc.edu>
2023-09-19 13:23:39 -0500

    verses changed

--- bottles.py
+++ bottles.py
@@ -13,4 +13,4 @@ print(count, "bottles of", beverage, "on the wall")
 print(count, "bottles of", beverage)
 print("Take one down, pass it around")
 count = count - 1
-print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage, "on the table")
