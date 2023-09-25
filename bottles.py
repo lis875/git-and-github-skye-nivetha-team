3e4a4eefc8c4a082dfaafcdbd1eb86cddf296ffd
NivethaVedantham3 <145385996+NivethaVedantham3@users.noreply.github.com>
2023-09-25 15:49:37 -0500

    Update bottles.py

--- bottles.py
+++ bottles.py
@@ -1,10 +1,17 @@
-beverage = "water"
+beverage = "Whiskey"
 count = 101
+
 for i in range(100):
     count = count - 1
     print(count, "bottles of", beverage, "on the wall")
-    print(count, "bottles of", "water")
-    print("If one of those bottles should happen to fall, ", count - 1, "bottles of beer on the wall...")
+    
+    if i % 2 == 0:
+        print(count, "bottles of", beverage)
+        print("Take one down, pass it around,", count - 1, "bottles of ", beverage, " on the wall...")
+    else:
+        print(count, "bottles of", beverage)
+        print("If one of those bottles should happen to fall,", count - 1, "bottles of",beverage, "on the wall...")
+    
     print("")
 
-print("No more bottles of beer on the wall, no more bottles of beer. We've taken them down and passed them around; now we're drunk and passed out!")
+print("No more bottles of ",beverage," on the wall, no more bottles of "+beverage+". We've taken them down and passed them around; now we're drunk and passed out!")
